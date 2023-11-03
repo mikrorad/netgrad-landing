79b513790c5954a1177dcb2a693d427ebda64930
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-11-03 15:28:49 +0100

    feat: finished landing page

--- src/consts.ts
+++ src/consts.ts
@@ -4,26 +4,26 @@
  * A few of these are not used yet, and are subject to change, example of this is Author.
  */
 export const SITE_METADATA = {
-    title: 'Astro Starter Blog',
+    title: 'NetGrad',
     headerTitle: 'NetGrad',
-    description: 'A blog created with Astro and Tailwind.css',
-    language: 'en-us',
-    theme: 'system', // Options: system, light, dark, Does not work yet
-    siteUrl: 'https://astro-starter-blog.yon.fun/',
-    siteRepo: 'https://github.com/wanoo21/tailwind-astro-starting-blog',
+    description: 'Kelola Jaringan Dengan Mudah',
+    language: 'id-ID',
+    theme: 'light', // Options: system, light, dark, Does not work yet
+    siteUrl: 'https://netgrad.id',
+    siteRepo: '',
     siteLogo: '/static/images/logo.png',
     socialBanner: '/static/images/twitter-card.png',
-    locale: 'en-US',
+    locale: 'id-ID',
 };
 
 /**
  * Default posts per page for pagination.
  */
-export const ITEMS_PER_PAGE = 5;
+export const ITEMS_PER_PAGE = 8;
 
 export const NAVIGATION = [
     {href: '/', title: 'Home'},
-    {href: '/blog', title: 'Blog'},
-    {href: '/contact', title: 'Contact'},
+    {href: '/tutorial', title: 'Tutorial'},
+    {href: '/contact', title: 'Kontak'},
     {href: 'https://app.netgrad.id', title: 'Dashboard'},
 ]
