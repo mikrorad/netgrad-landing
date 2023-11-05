14d3a93a5aeea3485a7889d69b01324a9a4ec6da
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-11-06 00:05:37 +0100

    feat: figure out which url to use based on build context

--- src/consts.ts
+++ src/consts.ts
@@ -3,21 +3,18 @@
  *
  * A few of these are not used yet, and are subject to change, example of this is Author.
  */
-const URL = process.env.CONTEXT === 'production' ? 'https://netgrad.id' : process.env.URL;
-const context = process.env.CONTEXT;
+const URL = process.env.CONTEXT === 'production' ? 'https://netgrad.id' : process.env.DEPLOY_PRIME_URL || 'http://localhost:4321';
 
 export const SITE_METADATA = {
-    title: 'NetGrad',
-    headerTitle: 'NetGrad',
-    description: 'Kelola Jaringan Dengan Mudah',
-    language: 'id-ID',
-    theme: 'light', // Options: system, light, dark, Does not work yet
-    siteUrl: URL,
-    siteRepo: '',
-    siteLogo: '/static/images/logo.png',
-    locale: 'id-ID',
-    context: context,
-    URL: process.env.DEPLOY_PRIME_URL,
+  title: 'NetGrad',
+  headerTitle: 'NetGrad',
+  description: 'Kelola Jaringan Dengan Mudah',
+  language: 'id-ID',
+  theme: 'light', // Options: system, light, dark, Does not work yet
+  siteUrl: URL,
+  siteRepo: '',
+  siteLogo: '/static/images/logo.png',
+  locale: 'id-ID',
 };
 
 /**
@@ -26,8 +23,8 @@ export const SITE_METADATA = {
 export const ITEMS_PER_PAGE = 8;
 
 export const NAVIGATION = [
-    {href: '/', title: 'Home'},
-    {href: '/tutorial', title: 'Tutorial'},
-    {href: '/contact', title: 'Kontak'},
-    {href: 'https://app.netgrad.id', title: 'Dashboard'},
+  { href: '/', title: 'Home' },
+  { href: '/tutorial', title: 'Tutorial' },
+  { href: '/contact', title: 'Kontak' },
+  { href: 'https://app.netgrad.id', title: 'Dashboard' },
 ]
