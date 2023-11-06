1f8f4e6ba13fde5405816b2a07826f661e0f8846
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2023-11-06 01:28:28 +0100

    feat: render opengraph images with pupeteer

--- src/pages/thumbnail/[title].png.ts
+++ src/pages/thumbnail/[title].png.ts
@@ -1,106 +1,35 @@
-import fs from "fs/promises";
+import fs from "fs";
 import satori from "satori";
 import sharp from "sharp";
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
-
 import { SITE_METADATA } from '@/consts';
 
+import puppeteer from 'puppeteer-core';
+import chromium from 'chrome-aws-lambda';
+
 export const GET: APIRoute = async function GET({ params, request }) {
-  const robotoData = await fs.readFile("./src/assets/InterBold.ttf");
-  // const image = await fs.readFile("./src/assets/og-image.png");
-  const myImageBase64 = (await fs.readFile("./src/assets/radiant-gradient.png")).toString("base64");
-  const title = params.title;
+  const localchrome = '/usr/bin/google-chrome-stable';
+  const executable = fs.existsSync(localchrome) ? localchrome : chromium.executablePath;
 
-  const html = {
-    type: "div",
-    props: {
-      style: {
-        display: "flex",
-        height: "100%",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        letterSpacing: "-.02em",
-        fontWeight: 700,
-        backgroundImage: `url('data:image/png;base64,${myImageBase64}')`
-      },
-      children: [
-        {
-          type: "div",
-          props: {
-            style: {
-              left: 42,
-              top: 42,
-              position: "absolute",
-              display: "flex",
-              alignItems: "center",
-            },
-            children: [
-              {
-                type: "span",
-                props: {
-                  style: {
-                    width: 24,
-                    height: 24,
-                    background: "black",
-                  },
-                },
-              },
-              {
-                type: "span",
-                props: {
-                  style: {
-                    marginLeft: 8,
-                    fontSize: 20,
-                  },
-                  children: "netgrad.id",
-                },
-              },
-            ],
-          },
-        },
-        {
-          type: "div",
-          props: {
-            style: {
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-              padding: "20px 50px",
-              margin: "0 42px",
-              fontSize: 40,
-              width: "auto",
-              maxWidth: 550,
-              textAlign: "center",
-              backgroundColor: "black",
-              color: "white",
-              lineHeight: 1.4,
-            },
-            children: title,
-          },
-        },
-      ],
-    },
-  };
-
-  const svg = await satori(
-    html,
-    {
-      width: 800,
-      height: 400,
-      fonts: [
-        {
-          name: "Roboto",
-          data: robotoData,
-          weight: "normal",
-          style: "normal",
-        },
-      ],
+  const browser = await puppeteer.launch({
+    executablePath: executable,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+    defaultViewport: {
+      width: 1200, height: 630
     }
-  );
+  });
 
-  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+  const page = await browser.newPage();
+
+  const content = await fs.readFileSync('./og-template.html').toString();
+
+  await page.setContent(content,{
+    waitUntil: 'domcontentloaded'
+  });
+
+  const png = (await page.screenshot());
 
   return new Response(png, {
     headers: {
